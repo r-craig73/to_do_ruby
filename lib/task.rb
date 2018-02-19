76238ffc9854b748b8efe422d6bc1538c4c6e9ad
Misha Petrov and Ron Craig <ron@>
2018-02-19 13:05:42 -0800

    troubleshoot the list specs

--- lib/task.rb
+++ lib/task.rb
@@ -2,14 +2,11 @@
 
 class Task
 
-  attr_reader(:description)
+  attr_reader(:description, :list_id)
 
   def initialize(attributes)
     @description = attributes.fetch(:description)
-  end
-
-  def ==(another_task)
-    self.description().==(another_task.description)
+    @list_id = attributes.fetch(:list_id)
   end
 
   def self.all
@@ -17,13 +14,17 @@ class Task
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
-      tasks.push(Task.new({:description => description}))
+      list_id = task.fetch("list_id").to_i()
+      tasks.push(Task.new({:description => description, :list_id => list_id}))
     end
     tasks
   end
 
   def save
-    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
   end
-  
+
+  def ==(another_task)
+    self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -11,28 +11,46 @@ RSpec.configure do |config|
   end
 end
 
-describe(List) do
+describe(Task) do
   describe(".all") do
-    it("starts off with no lists") do
-      expect(List.all()).to(eq([]))
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
     end
   end
-end
 
-describe(Task) do
+  describe("#save") do
+    it("adds a task to the array") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe("#description") do
+    it("lets you read the decription out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
+    end
+  end
+
+  describe("#list_id") do
+    it("lets you read the list ID out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
       expect(task1).to(eq(task2))
     end
   end
-end
 
-describe(Task) do
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => "learn SQL"})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
