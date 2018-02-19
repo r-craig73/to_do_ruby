bb363b33aa17c2a3e5e6e7f0658d6eac9c883aed
Misha Petrov and Ron Craig <ron@>
2018-02-19 13:08:48 -0800

    separate specs with adding spec_helper file

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,15 +1,8 @@
 require('rspec')
 require('list')
+require("spec_helper")
 require('pry')
 require('pg')
-
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM lists *;")
-  end
-end
 
 describe(List) do
   describe(".all") do
---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('task')
+require('list')
+require('pry')
+require('pg')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+    DB.exec("DELETE FROM lists *;")
+  end
+end
