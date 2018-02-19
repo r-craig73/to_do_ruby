f24b9cee7916d0101b51e95b9aecce65f155e6ac
Misha Petrov and Ron Craig <ron@>
2018-02-19 15:04:33 -0800

    pass first integration spec

--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**.*.rb')
+require('./lib/task')
+require('./lib/list')
 require('pry')
 require('pg')
 
 DB = PG.connect({:dbname => "to_do"})
 
-# get('/') do
-#   erb(:input)
-# end
-#
-# get('/output') do
-#   erb(:output)
-# end
+get('/') do
+  erb(:home)
+end
+
+post('/') do
+  name = params.fetch("list")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:success)
+end
